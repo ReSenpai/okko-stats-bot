cebd17e4f1dd5ab81822b7f5a34d19ebb83cccff
Denis <57324282+ReSenpai@users.noreply.github.com>
2020-12-30 18:10:30 +0300

    Update ctxValidators.ts

--- src/shared/validators/ctxValidators.ts
+++ src/shared/validators/ctxValidators.ts
@@ -1,3 +1,14 @@
 import { TelegrafContext } from "telegraf/typings/context";
 
+export const composeValidators = (...validators: Array<Function>) => (ctx: TelegrafContext) =>
+  validators.reduce((accum, validator) => accum || validator(ctx), false);
+
+
 export const isPrivateChat = (ctx: TelegrafContext) => ctx.chat?.type === 'private';
+export const msgTextValidator = (ctx: TelegrafContext) => ctx.message?.text ? true : false;
+export const msgReplyValidator = (ctx: TelegrafContext) => ctx.message?.reply_to_message && true;
+export const msgReplyTextValidator = (ctx: TelegrafContext) => {
+  if (ctx.message?.reply_to_message && ctx.message.reply_to_message?.text) return true;
+  return false;
+}
+
