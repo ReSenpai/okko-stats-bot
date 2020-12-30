35047ab22df2b91a189ade6ad84e21522c81dd29
Denis <57324282+ReSenpai@users.noreply.github.com>
2020-12-30 18:11:11 +0300

    Create session.ts

---
+++ src/middlewares/session.ts
@@ -0,0 +1,19 @@
+import LocalSession from 'telegraf-session-local';
+
+declare module 'telegraf/typings/context' {
+    export interface TelegrafContext {
+        session : {
+            buttonId: string,
+            menuId: number,
+            chatId: number
+        }
+    }
+}
+
+const config = {
+    database: 'sessions.json'
+}
+
+const session = () => new LocalSession(config).middleware();
+
+export default session;
