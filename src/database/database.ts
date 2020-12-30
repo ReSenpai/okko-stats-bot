4c8c3ca825ce5d7431afabe04d69be876dddca56
Denis <57324282+ReSenpai@users.noreply.github.com>
2020-12-30 18:05:54 +0300

    Update db config

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,7 +8,8 @@ const uri: string = process.env.CONNECTION_STRING || '';
 const connectToDb = () => mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true
 })
 .then(() => logg.green(1, 'MongoDB', 'Connected'))
 .catch(error => logg.error(2, 'MongoDB Connected', error));
