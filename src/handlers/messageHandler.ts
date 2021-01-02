d7c8341a48a9c5d3fe69e338c4fafc42ee95532a
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-02 20:31:20 +0300

    Refactoring messageHandler

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -3,34 +3,31 @@ import { TelegrafContext } from "telegraf/typings/context";
 import updateCategory from "../database/queryes/updateCategory";
 import createCategory from "./createCategory";
 import logg from '../utils/logger';
-import getMenuData from '../shared/getMenuData';
+import menuEditor from '../keyboards/menuEditor';
+import mainMenu from '../keyboards/mainMenu';
+import deleteMsgById from '../shared/actions/deleteMsgById';
 
 const msgReplyHandler = async (ctx: TelegrafContext) => {
     const replyMsgText = ctx.message?.reply_to_message?.text;
     const replyMsgId = ctx.message?.reply_to_message?.message_id;
     const msgId = ctx.message?.message_id;
 
+    const cleanUpMsg = () => {
+        deleteMsgById(ctx, replyMsgId);
+        deleteMsgById(ctx, msgId);
+    }
+
     switch (replyMsgText) {
         case 'Напишите название кнопки':
             await createCategory(ctx);
-            const {stats , kb} = await getMenuData();
-            try {
-                ctx.deleteMessage(replyMsgId);
-                ctx.deleteMessage(msgId);
-                ctx.telegram.editMessageText(
-                    ctx.session.chatId, 
-                    ctx.session.menuId, 
-                    ctx.session.menuId.toString(),
-                    stats,
-                    kb
-                );
-            } catch (error) {
-                console.log(error);
-            }
+            await mainMenu(ctx, 'edit-by-id');
+            cleanUpMsg();
             break;
         case 'Напишите новое имя для кнопки':
             if (!ctx.message?.text) return logg.error(2, 'messageHandler', 'ctx.message?.text undefined');
             await updateCategory(ctx.session.buttonId, ctx.message?.text);
+            await menuEditor(ctx, 'edit-by-id');
+            cleanUpMsg();
             break;
     }
 }
