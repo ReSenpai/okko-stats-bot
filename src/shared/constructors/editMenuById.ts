c45f1cacd29549c5a455b26302910daf87fe3996
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-02 20:27:01 +0300

    Create editMenuById.ts

---
+++ src/shared/constructors/editMenuById.ts
@@ -0,0 +1,14 @@
+import { TelegrafContext } from "telegraf/typings/context";
+import { ExtraEditMessage } from "telegraf/typings/telegram-types";
+
+const editMenuInSession = (ctx: TelegrafContext, menuText: string, keyboard: ExtraEditMessage) => {
+    return ctx.telegram.editMessageText(
+        ctx.session.chatId, 
+        ctx.session.menuId, 
+        ctx.session.menuId.toString(),
+        menuText,
+        keyboard
+    );
+}
+
+export default editMenuInSession;
