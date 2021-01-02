fae1cd9b4bd5ca33ca31c75ec08abcc28f76a5ce
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-02 20:25:54 +0300

    Fix validators

--- src/shared/validators/ctxValidators.ts
+++ src/shared/validators/ctxValidators.ts
@@ -1,14 +1,10 @@
 import { TelegrafContext } from "telegraf/typings/context";
 
-export const composeValidators = (...validators: Array<Function>) => (ctx: TelegrafContext) =>
-  validators.reduce((accum, validator) => accum || validator(ctx), false);
-
+export const composeValidators = (...validators: Array<Function>) => (ctx: TelegrafContext) => (
+  validators.reduce((accum, validator) => !validator(ctx) ? false : accum, true)
+);
 
 export const isPrivateChat = (ctx: TelegrafContext) => ctx.chat?.type === 'private';
-export const msgTextValidator = (ctx: TelegrafContext) => ctx.message?.text ? true : false;
-export const msgReplyValidator = (ctx: TelegrafContext) => ctx.message?.reply_to_message && true;
-export const msgReplyTextValidator = (ctx: TelegrafContext) => {
-  if (ctx.message?.reply_to_message && ctx.message.reply_to_message?.text) return true;
-  return false;
-}
-
+export const msgTextValidator = (ctx: TelegrafContext) => !!ctx.message?.text;
+export const msgReplyValidator = (ctx: TelegrafContext) => !!ctx.message?.reply_to_message;
+export const msgReplyTextValidator = (ctx: TelegrafContext) => !!ctx.message?.reply_to_message?.text;
