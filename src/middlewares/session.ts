fa428721fbab9d1a8c7fd5cd23aa2c9a27fec2eb
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-05 00:43:41 +0300

    Update session.ts

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,11 +1,22 @@
 import LocalSession from 'telegraf-session-local';
 
+export enum EAddCategoryStatus {
+    Name = 'name',
+    TimeLapse = 'timeLapse',
+    Limit = 'limit',
+    Done = 'done'
+}
+
 declare module 'telegraf/typings/context' {
     export interface TelegrafContext {
         session : {
-            buttonId: string,
-            menuId: number,
+            buttonId: string
+            menuId: number
             chatId: number
+            addCategoryStatus: EAddCategoryStatus | null
+            addCategoryName: string
+            addCategoryTimeLapse: number
+            addCategoryLimit: number
         }
     }
 }
