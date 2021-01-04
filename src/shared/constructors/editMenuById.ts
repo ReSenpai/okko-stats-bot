a7d2bf51f0a07ddd378bdc9f73a29cae715b5b70
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-04 16:14:49 +0300

    Update editMenuById.ts

--- src/shared/constructors/editMenuById.ts
+++ src/shared/constructors/editMenuById.ts
@@ -1,15 +1,19 @@
 import { TelegrafContext } from "telegraf/typings/context";
 import { ExtraEditMessage } from "telegraf/typings/telegram-types";
+import logg from "../../utils/logger";
 
 const editMenuInSession = (ctx: TelegrafContext, menuText: string, keyboard: ExtraEditMessage) => {
-    console.log(ctx.session);
-    return ctx.telegram.editMessageText(
-        ctx.session.chatId, 
-        ctx.session.menuId, 
-        ctx.session.menuId.toString(),
-        menuText,
-        keyboard
-    );
+    try {
+        return ctx.telegram.editMessageText(
+            ctx.session.chatId, 
+            ctx.session.menuId, 
+            ctx.session.menuId.toString(),
+            menuText,
+            keyboard
+        )
+    } catch (error) {
+        logg.error(2, 'editMenuInSession', error.code);
+    }  
 }
 
 export default editMenuInSession;
