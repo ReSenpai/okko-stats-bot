058779bc18a39465c8de58c047bb17763ce3fc2c
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-05 00:43:06 +0300

    Create messageReplyHandler.ts

---
+++ src/handlers/messageHandler/messageReplyHandler.ts
@@ -0,0 +1,34 @@
+import { TelegrafContext } from "telegraf/typings/context";
+import updateCategory from "../../database/queryes/updateCategory";
+import menuEditor from "../../keyboards/menuEditor";
+import cleanUpMsg from "../../shared/actions/cleanUpMsg";
+import logg from "../../utils/logger";
+import addCategoryHandler from "./addCategoryHandler";
+import inputTokenHandler from './inputTokenHandker';
+
+const msgReplyHandler = async (ctx: TelegrafContext) => {
+    const replyMsgText = ctx.message?.reply_to_message?.text;
+    const msgText = ctx.message?.text;
+    const userId = ctx.from?.id;
+    
+    if (!(msgText && userId)) return logg.error(2, 'messageHandler', 'ctx.message?.text undefined');
+
+    switch (replyMsgText) {
+        case 'Напишите название кнопки':
+            await addCategoryHandler(ctx);  
+            break;
+        case 'Напишите новое имя для кнопки':
+            await updateCategory(ctx.session.buttonId, msgText);
+            await menuEditor(ctx, 'edit-by-id');
+            cleanUpMsg(ctx);
+            break;
+        case 'Введите токен':
+            await inputTokenHandler(ctx);
+            break;
+        default:
+            await addCategoryHandler(ctx);
+            break;
+    }
+}
+
+export default msgReplyHandler;
