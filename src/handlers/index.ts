e14541529560cdc1947aec629a0c24e7916c8724
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-05 00:43:25 +0300

    Create index.ts

---
+++ src/handlers/index.ts
@@ -0,0 +1,5 @@
+export { default as authHandler } from './authHandler/authHandler';
+export { default as callbackQueryHandler } from './callbackQueryHandler/callbackQueryHandler';
+export { default as getTokenHandler } from './getTokenHandler/getTokenHandler';
+export { default as messageHandler } from './messageHandler/messageHandler';
+export { default as startHandler } from './startHandler/startHandler';
