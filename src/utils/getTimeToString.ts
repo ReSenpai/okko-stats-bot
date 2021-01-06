25e56a5caaac43938cd09379242b673e82757e9f
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-06 23:46:20 +0300

    Fixed time zone

--- src/utils/getTimeToString.ts
+++ src/utils/getTimeToString.ts
@@ -1,10 +1,19 @@
+import { config } from 'dotenv';
+config();
+
+const RESENPAI_DEV: string = process.env.RESENPAI_DEV || 'SERVER';
+
 const getTimeToString = (date: Date = new Date()) => {
-    const hours: string = ('0' + date.getHours()).slice(-2);
-    const minutes: string = ('0' + date.getUTCMinutes()).slice(-2);
-    const seconds: string = ('0' + date.getUTCSeconds()).slice(-2);
-    const days: string = ('0' + date.getDate()).slice(-2);
-    const months: string = ('0' + (date.getMonth() + 1)).slice(-2);
-    const years: number = date.getFullYear();
+    const currentDate = RESENPAI_DEV === 'DEV'
+    ? date
+    : new Date(date.setHours(date.getHours() + 3)); // 3 hours ahead
+
+    const hours: string = ('0' + currentDate.getHours()).slice(-2);
+    const minutes: string = ('0' + currentDate.getUTCMinutes()).slice(-2);
+    const seconds: string = ('0' + currentDate.getUTCSeconds()).slice(-2);
+    const days: string = ('0' + currentDate.getDate()).slice(-2);
+    const months: string = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+    const years: number = currentDate.getFullYear();
     return `${days}.${months}.${years}, ${hours}:${minutes}:${seconds}`;
 }
 
