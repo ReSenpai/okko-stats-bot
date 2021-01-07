dcb080602c336a18eb28abc82f9ac2b658df8a5d
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-07 22:47:35 +0300

    Fixed time zone logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -65,13 +65,17 @@ class Logger {
      */
     _getTime(): string {
         const date: Date = new Date();
-        const hours: string = ('0' + date.getHours()).slice(-2);
-        const minutes: string = ('0' + date.getUTCMinutes()).slice(-2);
-        const seconds: string = ('0' + date.getUTCSeconds()).slice(-2);
-        const days: string = ('0' + date.getDate()).slice(-2);
-        const months: string = ('0' + (date.getMonth() + 1)).slice(-2);
-        const years: number = date.getFullYear();
-        return `${days}.${months}.${years} / ${hours}:${minutes}:${seconds} `;
+        const currentDate = RESENPAI_DEV === 'DEV'
+        ? date
+        : new Date(date.setHours(date.getHours() + 3)); // 3 hours ahead
+
+        const hours: string = ('0' + currentDate.getHours()).slice(-2);
+        const minutes: string = ('0' + currentDate.getUTCMinutes()).slice(-2);
+        const seconds: string = ('0' + currentDate.getUTCSeconds()).slice(-2);
+        const days: string = ('0' + currentDate.getDate()).slice(-2);
+        const months: string = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+        const years: number = currentDate.getFullYear();
+        return `${days}.${months}.${years}, ${hours}:${minutes}:${seconds}`;
     }
     /**
      * Console log white
