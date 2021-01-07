587543486d9cfca27b8372cb31165a3e4a907f04
Denis <57324282+ReSenpai@users.noreply.github.com>
2021-01-07 22:49:12 +0300

    Fixed addCategoru query

--- src/database/queryes/addCategory.ts
+++ src/database/queryes/addCategory.ts
@@ -9,11 +9,8 @@ const createCategoryObj = (name: string, authorId: number) => ({
 
 const addCategory = async (name: string, authorId: number) => {
     try {
-        await new CategoryModel(createCategoryObj(name, authorId))
-        .save()
-        .then(() => {
-            logg.debug(2, 'MongoDB add category', name);
-        });
+        await new CategoryModel(createCategoryObj(name, authorId)).save();
+        logg.debug(2, 'MongoDB add category', name);
     } catch (error) {
         logg.error(2, 'MongoDB add category', error);
     }
